e26e3fb97f05d84c63448c70cfc9d11cabdc71be
Lars Michael Kristensen <lmkr@hvl.no>
2019-10-23 21:04:52 +0200

    HTML tester

---
+++ src/no/hvl/dat100/jplab12/tests/TestHtmlBlogg.java
@@ -0,0 +1,31 @@
+package no.hvl.dat100.jplab12.tests;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+
+import no.hvl.dat100.jplab12.oppgave2.Bilde;
+import no.hvl.dat100.jplab12.oppgave2.Tekst;
+import no.hvl.dat100.jplab12.oppgave4.SkrivFil;
+import no.hvl.dat100.jplab12.oppgave6.HtmlBlogg;
+
+class TestHtmlBlogg {
+
+private static String FILNAVN = "blogg.html";
+	
+	@Test
+	public void testskriv() {
+		
+		Tekst innlegg1 = new Tekst(1,"Ole Olsen","23-10-2019","DAT100 oppgave 6");
+		Bilde innlegg2 = new Bilde(2,"Oline Olsen","24-10-2019","HVL logo","https://www.west-norway.no/wp-content/uploads/2018/05/HVL-nettside.jpg");
+		
+		HtmlBlogg samling = new HtmlBlogg();
+		
+		samling.leggTil(innlegg1);
+		samling.leggTil(innlegg2);
+		
+		assertTrue(SkrivFil.skriv(samling, FILNAVN));
+	}	
+}
